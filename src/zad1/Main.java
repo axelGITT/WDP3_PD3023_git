d839297923f27e846ba9d83d707ee5e62b407d37
axel <axel_87@o2.pl>
2019-04-30 14:36:41 +0200

    Dodane tresci zadań w komentarzach

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -4,6 +4,25 @@
  *
  */
 
+/**
+ *  W pliku {user.home}/iter.txt podane są trzy liczby całkowite: start end limit(rozdzielone spacjami)
+Napisać program wczytujący te trzy liczby i sumujący  liczby całkowite od start do end, ale przerywającą sumowanie gdy wartość sumy osiągnie lub przekroczy zadane ograniczenie limit. 
+Program ma wypisać na konsoli jedną liczbę: wynik sumowania.
+
+Dla przykładu, jeśli w pliku  podano:
+1 3 6
+to na konsoli uzyskamy:
+6
+
+W przypadku wystąpienia błędów (np. brak pliku, wadliwe dane) należy wypisać na konsoli TYLKO trzy gwiazdki:
+
+***
+i zakończyć działanie programu.
+
+Nazwa pliku, jego umiejscowienie i postać oraz postać wyniku na konsoli jest obowiązkowa. Rozwiązanie nie spełniające tych wymagań otrzymują 0 punktów.
+ *
+ */
+
 package zad1;
 
 
